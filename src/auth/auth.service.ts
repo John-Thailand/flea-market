10bfa83bc2c9d5acab47a9ce22fffa5ffbb49a04
John-Thailand <naoki.honda@mirrorfit.jp>
2024-07-25 18:51:03 +0900

    ログイン機能の実装

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserRepository } from './user.repository';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from 'src/entities/user.entity';
+import { JwtService } from '@nestjs/jwt';
+import { CredentialsDto } from './dto/credentials.dto';
+
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
-  constructor(private userRepository: UserRepository) {}
+  constructor(
+    private userRepository: UserRepository,
+    private jwtService: JwtService,
+  ) {}
 
   async signUp(createUserDto: CreateUserDto): Promise<User> {
     return await this.userRepository.createUser(createUserDto);
+  }
+
+  async signIn(
+    credentialsDto: CredentialsDto,
+  ): Promise<{ accessToken: string }> {
+    const { username, password } = credentialsDto;
+    const user = await this.userRepository.findOne({ username });
+
+    // 平文のpasswordとハッシュ化されたuser.passwordを比較し、同じパスワードか判定する
+    if (user && (await bcrypt.compare(password, user.password))) {
+      // IDと名前のペイロードを作成
+      const payload = { id: user.id, username: user.username };
+      // ペイロードからアクセストークンを作成
+      const accessToken = await this.jwtService.sign(payload);
+      return { accessToken };
+    }
+    throw new UnauthorizedException(
+      'ユーザー名またはパスワードを確認してください',
+    );
   }
 }
